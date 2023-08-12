d8a8c251a521636a345982bcf0f81f6339a44644
Mohammed Chbani <mohamedachbani5@gmail.com>
2023-08-12 20:25:21 +0100

    first commit

---
+++ src/main/java/ma/ens/AviCultureBackend/AviCultureBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.ens.AviCultureBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviCultureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviCultureBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ens/AviCultureBackend/AviCultureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ens.AviCultureBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviCultureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
